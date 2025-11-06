bc4a78ccfefa0b14e28554ea79dce8486843275e
Tishanth-07 <139070316+Tishanth-07@users.noreply.github.com>
2025-11-06 18:04:47 +0530

    feat: implement HTTP API and core quiz server logic

--- backend/src/server/HttpServer.java
+++ backend/src/server/HttpServer.java
@@ -1,6 +1,10 @@
 package server;
 
 import com.sun.net.httpserver.*;
+import server.questions.QuestionManager;
+import server.questions.Question;
+import com.google.gson.Gson;
+
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.util.List;
@@ -10,9 +14,15 @@ public class HttpServer {
     private static final int HTTP_PORT = 8080;
 
     public void start() throws IOException {
+        // Load questions from backend/resources/questions.json before server starts
+        QuestionManager.loadQuestions();
+
         server = com.sun.net.httpserver.HttpServer.create(new InetSocketAddress(HTTP_PORT), 0);
         
-        // Enable CORS for all routes
+        // --- Question API ---
+        server.createContext("/api/questions", new QuestionsHandler());
+        
+        // --- Other APIs ---
         server.createContext("/api/students", new StudentsHandler());
         server.createContext("/api/quiz/start", new StartQuizHandler());
         server.createContext("/api/status", new StatusHandler());
@@ -28,11 +38,39 @@ public class HttpServer {
         }
     }
 
-    // Handler for /api/students - returns list of connected students
+    // ---  Handler for /api/questions ---
+    static class QuestionsHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+            exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, OPTIONS");
+            exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type");
+
+            if ("OPTIONS".equals(exchange.getRequestMethod())) {
+                exchange.sendResponseHeaders(204, -1);
+                return;
+            }
+
+            if ("GET".equals(exchange.getRequestMethod())) {
+                List<Question> questions = QuestionManager.getAllQuestions();
+                String jsonResponse = new Gson().toJson(questions);
+
+                byte[] response = jsonResponse.getBytes();
+                exchange.getResponseHeaders().set("Content-Type", "application/json");
+                exchange.sendResponseHeaders(200, response.length);
+                OutputStream os = exchange.getResponseBody();
+                os.write(response);
+                os.close();
+            } else {
+                exchange.sendResponseHeaders(405, -1);
+            }
+        }
+    }
+
+    // --- Handler for /api/students ---
     static class StudentsHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
-            // Enable CORS
             exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
             exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, OPTIONS");
             exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type");
@@ -44,8 +82,6 @@ public class HttpServer {
 
             if ("GET".equals(exchange.getRequestMethod())) {
                 List<ClientHandler> clients = QuizServer.getConnectedClients();
-                
-                // Build JSON response
                 StringBuilder json = new StringBuilder("[");
                 for (int i = 0; i < clients.size(); i++) {
                     ClientHandler client = clients.get(i);
@@ -70,7 +106,7 @@ public class HttpServer {
         }
     }
 
-    // Handler for /api/quiz/start - starts the quiz
+    // --- Handler for /api/quiz/start ---
     static class StartQuizHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
@@ -84,7 +120,6 @@ public class HttpServer {
             }
 
             if ("POST".equals(exchange.getRequestMethod())) {
-                // Broadcast START_QUIZ to all connected clients
                 List<ClientHandler> clients = QuizServer.getConnectedClients();
                 for (ClientHandler client : clients) {
                     client.send("START_QUIZ");
@@ -102,7 +137,7 @@ public class HttpServer {
         }
     }
 
-    // Handler for /api/status - returns server status
+    // --- Handler for /api/status ---
     static class StatusHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
